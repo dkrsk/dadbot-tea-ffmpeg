b10cce63be9e1c39264936219e136ed7385caa4a
dkrsk <wileps2310@yandex.ru>
2023-02-26 02:22:18 +0300

    fix file location ?

--- init.sh
+++ init.sh
@@ -3,12 +3,9 @@ if [ ! -d /data ]; then
 	mkdir /data
 fi
 
-if [ ! -d /app ]; then
-	mv ./config.json /data/config.json
-	mv ./pasts.txt /data/pasts.txt
-else
-	mv /app/config.json /data/config.json
-	mv /app/pasts.txt /data/pasts.txt
+if [ -d /app ]; then
+	mv /app/data/config.json /data/config.json
+	mv /app/data/pasts.txt /data/pasts.txt
 fi
  
 python3 ./bot.py
