92ef797ee9eb69d36fbb44cd84f38016f9bb4874
dkrsk <wileps2310@yandex.ru>
2023-02-26 02:06:04 +0300

    add ability to deploy to heroku

--- init.sh
+++ init.sh
@@ -2,8 +2,13 @@
 if [ ! -d /data ]; then
 	mkdir /data
 fi
-mv /app/config.json /data/config.json
-mv /app/pasts.txt /data/pasts.txt
 
+if [ ! -d /app ]; then
+	mv ./config.json /data/config.json
+	mv ./pasts.txt /data/pasts.txt
+else
+	mv /app/config.json /data/config.json
+	mv /app/pasts.txt /data/pasts.txt
+fi
  
 python3 ./bot.py
